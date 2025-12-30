c59253639cc33df398a5c2bf72613fcca99445e4
phanatcha <phanatcha19@gmail.com>
2025-12-30 23:24:43 +0700

    new file:   public/icons/eslint-icon.svg 	new file:   public/icons/playwright-icon.svg 	new file:   public/icons/react-icon.svg 	new file:   public/icons/react_hook_form-icon.svg 	new file:   public/icons/tailwind-icon.svg 	new file:   public/icons/tanstack_query-icon.svg 	new file:   public/icons/typescript-icon.svg 	new file:   public/icons/vite-icon.svg 	new file:   public/icons/vitest-icon.svg 	new file:   public/icons/zustand-icon.svg 	deleted:    public/vite.svg 	modified:   src/routes/Home.tsx 	modified:   src/styles/index.css 	modified:   tailwind.config.ts

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,3 +1,22 @@
+import EslintIcon from '/icons/eslint-icon.svg';
+// import PlaywrightIcon from '/icons/playwright-icon.svg';
+// import ReactHookFormIcon from '/icons/react_hook_form-icon.svg';
+import React from 'react';
+import ReactIcon from '/icons/react-icon.svg';
+// import TailwindIcon from '/icons/tailwind-icon.svg';
+// import TanStackQueryIcon from '/icons/tanstack_query-icon.svg';
+// import TypescriptIcon from '/icons/typescript-icon.svg';
+// import ViteIcon from '/icons/vite-icon.svg';
+// import VitestIcon from '/icons/vitest-icon.svg';
+// import ZustandIcon from './icons/zustand-icon.svg';
+
 export default function Home() {
-  return <h1>Coming Soon</h1>;
+  return (
+    <div>
+      <header className="relative h-24">
+        <h1>Coming Soon!</h1>
+      </header>
+    </div>
+  )
 }
+
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,10 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: ["./index.html", "./src/**/*.{ts,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
