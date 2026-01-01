2d42a65058c3a9093e729f754f2525ad8030c691
phanatcha <phanatcha19@gmail.com>
2026-01-01 12:53:39 +0700

    new file:   src/hooks/FadeRise.tsx 	modified:   src/routes/Home.tsx

---
+++ src/hooks/FadeRise.tsx
@@ -0,0 +1,20 @@
+import { useEffect, useRef, useState } from "react";
+
+export function useInView() {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [visible, setVisible] = useState(false);
+
+    useEffect(() => {
+    const observer = new IntersectionObserver(
+        ([entry]) => {
+        setVisible(entry.isIntersecting);
+        },
+        { threshold: 0.3 }
+    );
+
+    if (ref.current) observer.observe(ref.current);
+    return () => observer.disconnect();
+    }, []);
+
+  return { ref, visible };
+}
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -9,8 +9,11 @@ import TypescriptIcon from '/icons/typescript-icon.svg';
 import ViteIcon from '/icons/vite-icon.svg';
 import VitestIcon from '/icons/vitest-icon.svg';
 import ZustandIcon from '/icons/zustand-icon.svg';
+import { useInView } from '../hooks/FadeRise';
+
 
 export default function Home() {
+  const { ref, visible } = useInView();
   return (
     <div className="bg-black text-white min-h-screen flex flex-col">
       <header>
@@ -71,156 +74,186 @@ export default function Home() {
 
       </header>
 
-      <main className="flex flex-col bg-[#3C6A77] items-center justify-center min-h-screen p-10 space-y-10">
-        <h2 className="text-4xl font-bold lg:text-6xl md:text-5xl">
-          Tech Stack
-        </h2>
-
-        <p className="text-xl leading-8 max-w-3xl">
-          Built with modern tools to help you ship faster and scale with confidence.
-        </p>
-        <section className="flex m-5 space-x-5 justify-center items-center">
-          <img 
-            src={ReactIcon} 
-            alt="React Icon" 
-            className="h-13 w-13 object-contain" 
-          />
-          <p className="text-2xl font-medium">React</p>
-
-          <img 
-            src={TypescriptIcon} 
-            alt="Typescript Icon" 
-            className="h-11 w-11 object-contain" 
-          />
-          <p className="text-2xl font-medium">Typescript</p>
-
-          <img 
-            src={ViteIcon} 
-            alt="Vite Icon" 
-            className="h-11 w-11 object-contain" 
-          />
-          <p className="text-2xl font-medium">Vite</p>
-
-        </section>
-
-        <section className="flex flex-col md:flex-row md:space-x-10 space-y-10 md:space-y-0 justify-center items-center">
-
-          <div className="
-            w-80 h-80
-            md:w-64 md:h-64
-            rounded-2xl
-            bg-gradient-to-b from-[#0093BE] to-[#090F11]
-            shadow-xl shadow-black/40
-            border border-white/10
-            p-6
+      <main 
+        className="
+          relative h-[200vh]
+          bg-[#2c8ea9] 
+        "
+      >
+        <div
+          ref={ref}
+          className={`
+            sticky top-0
+            min-h-screen
             flex flex-col
-            items-center
-          ">
+            items-center justify-center
+            transition-all duration-1000 ease-out
+            ${visible
+              ? "opacity-100 translate-y-0"
+              : "opacity-0 translate-y-10"}
+          `}
+        >
 
-            <h3 className="text-3xl font-extrabold mb-4">
-              UI & UX
-            </h3>
+          <h2 className="text-4xl font-bold lg:text-6xl md:text-5xl">
+            Tech Stack
+          </h2>
 
-            <div className="flex space-x-2 mb-5">
-              <img 
-                src={TailwindIcon} 
-                alt="Tailwind Icon" 
-                className="h-7 w-7 object-contain" 
-              />
-              <p className="text-lg font-medium">Tailwind CSS</p>
-            </div>
-            <div className="flex space-x-2 mb-5">
-              <img 
-                src={ReactHookFormIcon} 
-                alt="React Hook Form Icon" 
-                className="h-7 w-7 object-contain" 
-              />
-              <p className="text-lg font-medium">React Hook Form</p>
-            </div>
+          <p className="text-xl leading-8 max-w-3xl">
+            Built with modern tools to help you ship faster and scale with confidence.
+          </p>
+          <section className="flex m-5 space-x-5 justify-center items-center">
+            <img 
+              src={ReactIcon} 
+              alt="React Icon" 
+              className="h-13 w-13 object-contain" 
+            />
+            <p className="text-2xl font-medium">React</p>
 
-          </div>
+            <img 
+              src={TypescriptIcon} 
+              alt="Typescript Icon" 
+              className="h-11 w-11 object-contain" 
+            />
+            <p className="text-2xl font-medium">Typescript</p>
 
-          <div className="
-            w-80 h-80
-            md:w-64 md:h-64
-            rounded-2xl
-            bg-gradient-to-b from-[#00D2DA] to-[#043F41]
-            shadow-xl shadow-black/40
-            border border-white/10
-            p-6
-            flex flex-col
-            items-center
-          ">
+            <img 
+              src={ViteIcon} 
+              alt="Vite Icon" 
+              className="h-11 w-11 object-contain" 
+            />
+            <p className="text-2xl font-medium">Vite</p>
 
-            <h3 className="text-3xl font-extrabold mb-4">
-              State & Data
-            </h3>
+          </section>
 
-            <div className="flex space-x-2 mb-5">
-              <img 
-                src={TanStackQueryIcon} 
-                alt="TanStack Query Icon" 
-                className="h-7 w-7 object-contain" 
-              />
-              <p className="text-lg font-medium">TanStack Query</p>
-            </div>
-            <div className="flex space-x-2 mb-5">
-              <img 
-                src={ZustandIcon} 
-                alt="Zustand Icon" 
-                className="h-7 w-7 object-contain" 
-              />
-              <p className="text-lg font-medium">Zustand</p>
-            </div>
+          <section className="flex flex-col md:flex-row md:space-x-10 space-y-10 md:space-y-0 justify-center items-center">
 
-          </div>
+            <div className="
+              w-80 h-80
+              md:w-64 md:h-64
+              rounded-2xl
+              bg-gradient-to-b from-[#0093BE] to-[#090F11]
+              shadow-xl shadow-black/40
+              border border-white/10
+              p-6
+              flex flex-col
+              items-center
+              transition-all duration-300 ease-out
+              hover:-translate-y-2
+              hover:shadow-2xl hover:shadow-black/60
+            ">
 
-          <div className="
-            w-80 h-80
-            md:w-64 md:h-64
-            rounded-2xl
-            bg-gradient-to-b from-[#96E7FF] to-[#006D8E]
-            shadow-xl shadow-black/40
-            border border-white/10
-            p-6
-            flex flex-col
-            items-center
-          ">
+              <h3 className="text-3xl font-extrabold mb-4">
+                UI & UX
+              </h3>
 
-            <h3 className="text-3xl font-extrabold mb-4">
-              Quality
-            </h3>
+              <div className="flex space-x-2 mb-5">
+                <img 
+                  src={TailwindIcon} 
+                  alt="Tailwind Icon" 
+                  className="h-7 w-7 object-contain" 
+                />
+                <p className="text-lg font-medium">Tailwind CSS</p>
+              </div>
+              <div className="flex space-x-2 mb-5">
+                <img 
+                  src={ReactHookFormIcon} 
+                  alt="React Hook Form Icon" 
+                  className="h-7 w-7 object-contain" 
+                />
+                <p className="text-lg font-medium">React Hook Form</p>
+              </div>
 
-            <div className="flex space-x-2 mb-5">
-              <img 
-                src={VitestIcon} 
-                alt="Vitest Icon" 
-                className="h-7 w-7 object-contain" 
-              />
-              <p className="text-lg font-medium">Vitest</p>
-            </div>
-            <div className="flex space-x-2 mb-5">
-              <img 
-                src={PlaywrightIcon} 
-                alt="Playwright Icon" 
-                className="h-7 w-7 object-contain" 
-              />
-              <p className="text-lg font-medium">Playwright</p>
             </div>
 
-            <div className="flex space-x-2 mb-5">
-              <img 
-                src={EslintIcon} 
-                alt="Eslint Icon" 
-                className="h-7 w-7 object-contain" 
-              />
-              <p className="text-lg font-medium">Eslint</p>
+            <div className="
+              w-80 h-80
+              md:w-64 md:h-64
+              rounded-2xl
+              bg-gradient-to-b from-[#00D2DA] to-[#043F41]
+              shadow-xl shadow-black/40
+              border border-white/10
+              p-6
+              flex flex-col
+              items-center
+              transition-all duration-300 ease-out
+              hover:-translate-y-2
+              hover:shadow-2xl hover:shadow-black/60
+            ">
+
+              <h3 className="text-3xl font-extrabold mb-4">
+                State & Data
+              </h3>
+
+              <div className="flex space-x-2 mb-5">
+                <img 
+                  src={TanStackQueryIcon} 
+                  alt="TanStack Query Icon" 
+                  className="h-7 w-7 object-contain" 
+                />
+                <p className="text-lg font-medium">TanStack Query</p>
+              </div>
+              <div className="flex space-x-2 mb-5">
+                <img 
+                  src={ZustandIcon} 
+                  alt="Zustand Icon" 
+                  className="h-7 w-7 object-contain" 
+                />
+                <p className="text-lg font-medium">Zustand</p>
+              </div>
+
             </div>
 
-          </div>
+            <div className="
+              w-80 h-80
+              md:w-64 md:h-64
+              rounded-2xl
+              bg-gradient-to-b from-[#96E7FF] to-[#006D8E]
+              shadow-xl shadow-black/40
+              border border-white/10
+              p-6
+              flex flex-col
+              items-center
+              transition-all duration-300 ease-out
+              hover:-translate-y-2
+              hover:shadow-2xl hover:shadow-black/60
+            ">
+
+              <h3 className="text-3xl font-extrabold mb-4">
+                Quality
+              </h3>
+
+              <div className="flex space-x-2 mb-5">
+                <img 
+                  src={VitestIcon} 
+                  alt="Vitest Icon" 
+                  className="h-7 w-7 object-contain" 
+                />
+                <p className="text-lg font-medium">Vitest</p>
+              </div>
+              <div className="flex space-x-2 mb-5">
+                <img 
+                  src={PlaywrightIcon} 
+                  alt="Playwright Icon" 
+                  className="h-7 w-7 object-contain" 
+                />
+                <p className="text-lg font-medium">Playwright</p>
+              </div>
+
+              <div className="flex space-x-2 mb-5">
+                <img 
+                  src={EslintIcon} 
+                  alt="Eslint Icon" 
+                  className="h-7 w-7 object-contain" 
+                />
+                <p className="text-lg font-medium">Eslint</p>
+              </div>
+
+            </div>
 
 
-        </section>
+          </section>
+
+        </div>
       </main>
 
       <footer className="flex flex-col items-center justify-end min-h-40 h-32 p-6">
